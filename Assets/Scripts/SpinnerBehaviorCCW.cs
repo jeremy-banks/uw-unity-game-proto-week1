d616e0cbd340baaef9ccc06783bd35200981d48f
Jeremy B <workjeremyb@gmail.com>
2024-07-09 21:38:02 -0700

    final

--- Assets/Scripts/SpinnerBehaviorCCW.cs
+++ Assets/Scripts/SpinnerBehaviorCCW.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallBehavior : MonoBehaviour
+public class SpinnerBehaviorCCW : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // add initial velocit
-        GetComponent<Rigidbody2D>().AddForce( new Vector2(500f, 500f));
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.Rotate(0, 0, 360f * Time.deltaTime);
     }
 }
