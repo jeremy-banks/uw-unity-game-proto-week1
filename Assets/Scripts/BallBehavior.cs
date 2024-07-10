c177482608db9a9d61340b6b911a3ccd2002b178
Jeremy B <workjeremyb@gmail.com>
2024-07-09 20:22:31 -0700

    webgl

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    // add initial velocity
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
